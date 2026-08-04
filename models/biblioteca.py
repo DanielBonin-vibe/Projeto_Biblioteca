024ddf7c12da4109c2b6e7b7696fc7c5bacc71ea
Daniel Bonin <danielbonin0011@gmail.com>
2026-08-04 15:23:35 -0300

    Adição da persistência de dados com JSON no Projeto em questão

--- models/biblioteca.py
+++ models/biblioteca.py
@@ -1,17 +1,19 @@
-
+from utils import persistencia
 
 class Biblioteca:
     def __init__(self):
-        self.livros = []
-        self.usuarios = []
+        self.livros = persistencia.carregar_livros()
+        self.usuarios = persistencia.carregar_usuarios()
 
     def adicionar_livro(self, livro):      # Vai receber o objeto Livro 
         self.livros.append(livro)          # Adiciona a lista 'self.livros' o objeto livro criado em livros.py
+        persistencia.salvar_livro(self.livros)
         print(f'O livro "{livro.titulo}" foi adicionado à biblioteca.')
 
         
-    def remover_livro(self, livro):
+    def remover_livros(self, livro):
         self.livros.remove(livro)
+        persistencia.salvar_livros(self.livros)
         print(f'O livro {livro.titulo} foi removido da nossa coletânea')
 
 
@@ -21,10 +23,13 @@ class Biblioteca:
 
     def cadastrar_usuario(self, usuario):   # Recebe o objeto 'usuario' 
         self.usuarios.append(usuario)       # Adiciona o objeto usuario a lista de usuarios 'self.usuarios'
+        persistencia.salvar_usuarios(self.usuarios)
         print(f'O usuário {usuario.nome} foi cadastrado.')
+
 
     def remover_usuario(self, usuario):
         self.usuarios.remove(usuario)
+        persistencia.salvar_usuarios(self.usuarios)
         print(f'O usuário {usuario.nome} foi removido do nosso cadastrado.')
 
     def listar_usuarios(self):
--- utils/persistencia.py
+++ utils/persistencia.py
@@ -1,18 +1,43 @@
 import json
-
-def salvar_alunos(alunos):
-
-    dados = []
-
-    for aluno in alunos:
-        dados.append(aluno.to_dict())
-
-    with open('dados/alunos.json', 'w', encoding='utf-8') as arquivo:
-        json.dump(dados, arquivo, indent=4, ensure_ascii=False)
-
-def salvar_professores(professores):
+from models import Usuario, Livro
+def salvar_usuarios(usuarios):
 
     dados = []
 
-    with open('dados/professores.json', 'w', encoding='utf-8') as arquivo:
+    for usuario in usuarios:
+        dados.append(usuario.to_dict())
+
+    with open('dados/usuarios.json', 'w', encoding='utf-8') as arquivo:
         json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+
+def salvar_livros(livros):
+
+    dados = []
+
+    for livro in livros:
+        dados.append(livro.to_dict())
+
+    with open('dados/livros.json', 'w', encoding='utf-8') as arquivo:
+        json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+
+
+def carregar_usuarios():
+    with open('dados/usuarios.json', 'r', encoding='utf-8') as arquivo:
+        dados = json.load(arquivo)
+
+    usuarios = []
+
+    for usuario in dados:
+        usuarios.append(Usuario.from_dict(usuario))
+        return usuarios
+
+
+def carregar_livros():
+    with open('dados/livros.json', 'r', encoding='utf-8') as arquivo:
+        dados = json.load(arquivo)
+
+        livros = []
+
+    for livro in dados:
+        livros.append(Livro.from_dict(livro))
+        return livros
